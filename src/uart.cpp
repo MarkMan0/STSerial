bec22dab4ae45099b7005b042df40955bb1a66a0
MarkMan0 <pal.mark28@gmail.com>
2022-06-11 14:20:32 +0200

    Refactor to make callbacks cleaner

--- src/uart.cpp
+++ src/uart.cpp
@@ -2,8 +2,7 @@
 #include "pin_api.h"
 
 
-UART_DMA::UART_DMA(hw_init_fcn_t* a, isr_enable_fcn_t* b, msp_init_fcn_t* c, rx_event_fcn_t* d)
-  : hw_init_cb(a), isr_enable_cb(b), msp_init_cb(c), rx_event_cb(d) {
+UART_DMA::UART_DMA(hw_init_fcn_t* a, isr_enable_fcn_t* b) : hw_init_cb(a), isr_enable_cb(b) {
 }
 
 void UART_DMA::begin(uint32_t baud) {
@@ -13,6 +12,9 @@ void UART_DMA::begin(uint32_t baud) {
 
   HAL_UART_Receive_DMA(&huart_, dma_buff_.data(), dma_buff_.size());
   huart_.ReceptionType = HAL_UART_RECEPTION_TOIDLE;  // also reacts to IDLE interrupt, so only one callback is needed
+
+  // enable IDLE interrupt
+  SET_BIT(huart_.Instance->CR1, USART_CR1_IDLEIE);
 }
 
 void UART_DMA::send(const char* buff, size_t sz) {
@@ -22,18 +24,19 @@ void UART_DMA::send(const char* buff, size_t sz) {
   }
 }
 
-void UART_DMA::tick() {
-  uint16_t n = transmit_buff_.get_occupied_continuous();
-  if (n) {
+void UART_DMA::flush() {
+  while (uint16_t n = transmit_buff_.get_occupied_continuous()) {
     HAL_UART_Transmit_DMA(&huart_, const_cast<uint8_t*>(&transmit_buff_.peek()), n);
     transmit_buff_.pop(n);
   }
 }
 
+void UART_DMA::tick() {
+  flush();
+}
 
 
-UART_DMA uart2(UART_DMA::uart2_hw_init, UART_DMA::uart2_enable_isrs, UART_DMA::uart2_msp_init,
-               UART_DMA::uart2_rx_event_cb);
 
-UART_DMA uart1(UART_DMA::uart1_hw_init, UART_DMA::uart1_enable_isrs, UART_DMA::uart1_msp_init,
-               UART_DMA::uart1_rx_event_cb);
+UART_DMA uart2(UART_DMA::uart2_hw_init, UART_DMA::uart2_enable_isrs);
+
+UART_DMA uart1(UART_DMA::uart1_hw_init, UART_DMA::uart1_enable_isrs);
--- src/uart.h
+++ src/uart.h
@@ -17,10 +17,8 @@ class UART_DMA {
 public:
   using hw_init_fcn_t = void(UART_DMA&);
   using isr_enable_fcn_t = void(UART_DMA&);
-  using msp_init_fcn_t = void(UART_HandleTypeDef*);
-  using rx_event_fcn_t = void(UART_HandleTypeDef*, uint16_t);
 
-  UART_DMA(hw_init_fcn_t*, isr_enable_fcn_t*, msp_init_fcn_t*, rx_event_fcn_t*);
+  UART_DMA(hw_init_fcn_t*, isr_enable_fcn_t*);
 
   void begin(uint32_t baud);
 
@@ -32,7 +30,10 @@ public:
     return receive_buff_.pop();
   }
 
-
+  /** @name Immediate transmission
+   *  @details Messages are sent out immediately
+   */
+  ///@{
   void transmit(uint8_t c) {
     HAL_UART_Transmit(&huart_, &c, 1, HAL_MAX_DELAY);
   }
@@ -42,31 +43,41 @@ public:
   void transmit(const char* str) {
     transmit(str, strlen(str));
   }
+  ///@}
 
-
+  /** @name transmit messages using buffer in tick()
+   *  @details Messages are placed into transmission_buffer and sent out later, asynchronously
+   */
+  ///@{
   void send(const char*, size_t);
   void send(const char* str) {
     send(str, strlen(str));
   }
 
-  void tick();
+  /// @brief Flush the transmission buffer
+  void flush();
+  ///@}
+
+  void tick();  //!< Called peridically to empty the transmit buffer
 
   UART_HandleTypeDef huart_;
   DMA_HandleTypeDef hdmarx_, hdmatx_;
 
 private:
   uint32_t baudrate_{ 115200 };
-  std::array<uint8_t, 64> dma_buff_;
-  RingBuffer<uint8_t, 64> receive_buff_;
-  RingBuffer<uint8_t, 64> transmit_buff_;
+  uint16_t last_rxdma_pos_{ 0 };           ///< Used in rx event callback to track DMA
+  std::array<uint8_t, 64> dma_buff_;       ///< Buffer sued by the DMA to receive
+  RingBuffer<uint8_t, 64> receive_buff_;   ///< Messages from DMA buffer are put here
+  RingBuffer<uint8_t, 64> transmit_buff_;  ///< Buffer for non-immediate transmission
 
   const hw_init_fcn_t* hw_init_cb;
   const isr_enable_fcn_t* isr_enable_cb;
-  const msp_init_fcn_t* msp_init_cb;
-  const rx_event_fcn_t* rx_event_cb;
 
 
 public:
+  /** @brief Generic RX event callback */
+  void rx_event_cb(UART_HandleTypeDef*, uint16_t);
+
   /** @name UART2 init and callback functions */
   ///@{
 
@@ -74,10 +85,10 @@ public:
   static void uart2_hw_init(UART_DMA&);
   /** @brief Enables interrupts for UART and DMAs */
   static void uart2_enable_isrs(UART_DMA&);
-  /** @brief Called by the HAL library to init the pins and DMA for UART2 */
-  static void uart2_msp_init(UART_HandleTypeDef*);
   /** @brief Called by HAL on DMA or IDLE interrupt */
   static void uart2_rx_event_cb(UART_HandleTypeDef* huart, uint16_t Pos);
+
+  ///@}
 
 
   /** @name UART1 init and callback functions */
@@ -87,8 +98,6 @@ public:
   static void uart1_hw_init(UART_DMA&);
   /** @brief Enables interrupts for UART and DMAs */
   static void uart1_enable_isrs(UART_DMA&);
-  /** @brief Called by the HAL library to init the pins and DMA for UART1 */
-  static void uart1_msp_init(UART_HandleTypeDef*);
   /** @brief Called by HAL on DMA or IDLE interrupt */
   static void uart1_rx_event_cb(UART_HandleTypeDef* huart, uint16_t Pos);
 
